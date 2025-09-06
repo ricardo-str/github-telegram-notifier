1c5b5325c026b7b2af1ce451ac205e0e2d4f0e3a
ricardo-str <ricardo.sepulveda.t@ug.uchile.cl>
2025-09-06 17:13:26 -0400

    update: mapeo de nombres de GitHub y Telegram actualizado

--- src/user-mapping.js
+++ src/user-mapping.js
@@ -1,14 +1,14 @@
 // Mapeo de usuarios GitHub a Telegram
+
 const USER_MAPPING = {
   // GitHub username → Telegram username (sin @)
   'ricardo-str': 'Ricardo_s_t',
-  // Añadir más miembros del equipo aquí:
-  // 'github-user1': 'telegram-user1',
-  // 'github-user2': 'telegram-user2',
-  // 'github-user3': 'telegram-user3',
-  // 'github-user4': 'telegram-user4',
-  // 'github-user5': 'telegram-user5',
-  // 'github-user6': 'telegram-user6',
+  'javiromeroo': 'jaavirome',
+  'Dagt07': 'Dagt07',
+  'edgarsmorg': 'edgar_smg',
+  'a-restrepo': 'andres_x11',
+  'JPFred': 'jpfred',
+  'VicenteZamora': 'vicentz',
 };
 
 module.exports = USER_MAPPING;
